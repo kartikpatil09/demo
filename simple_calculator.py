9d6d86d38cd759f5240e074aa9b86ec0c93827a0
kartikpatil09 <kartikamolpatil09@gmail.com>
2026-01-30 13:18:31 +0530

    Taking input is done

---
+++ simple_calculator.py
@@ -0,0 +1,11 @@
+print("----------------------------------SIMPLR CALCULATOR----------------------------------")
+print("Welcome to the Simple Calculator!")  
+print("You can perform the following operations:")
+print("1. Addition (+)") 
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter your choice(1-4) : ") 
+a = float(input("Enter the first number: "))
+b = float(input("enter the second number:"))
