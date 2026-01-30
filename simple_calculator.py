0d87852b0a0ca85537d13d110073babc14b671da
kartikpatil09 <kartikamolpatil09@gmail.com>
2026-01-30 13:56:58 +0530

    Printing Result

---
+++ simple_calculator.py
@@ -0,0 +1,10 @@
+if choice == 1 :
+    print("Result : " , add(a,b))
+elif choice == 2 :
+    print("Result : " , substract(a ,b))
+elif choice == 3 :
+    print("Result : " , multiply(a,b))
+elif choice == 4 :
+    print("Result : " , division(a,b))
+else :
+    print("Invalid choce : " , choice)
